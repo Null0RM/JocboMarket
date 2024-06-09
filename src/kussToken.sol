c42cd8088333b9ef4c611616476031cccc32ec6f
Nullorm <jhy_2301@naver.com>
2024-06-09 13:16:49 +0900

    initial set

---
+++ src/kussToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2024 Hyoyoung Jung
+pragma solidity ^0.8.0;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract KussToken is ERC20 {
+	
+}
